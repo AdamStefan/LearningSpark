e5184355ced54f2babeb3ed07083bc01f0f16c76
AdamStefan <AdamStefan2003@gmail.com>
2016-06-15 13:20:23 +0300

    code review

--- src/main/scala/com/sample/AppSpark.scala
+++ src/main/scala/com/sample/AppSpark.scala
@@ -38,48 +38,39 @@ object AppSpark {
     val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
     var noOfItems = counts.count()
     val items = counts.top(4)
+
+    //reduceToALine
     val reducedToALine = words.reduce((x, y) => x + y)
     println(reducedToALine)
+
+    // test  with Serialization using a custom search function
     val youSearchFunction = new SearchFunctions("o")
     val wordsWithO = youSearchFunction.getMatchesNoReference(words)
     val firstWord = wordsWithO.first()
+
+
+    // use parallelize collection
     val parallelizeCollection = sc.parallelize(List(1, 2, 3, 4))
     val sumSquareValues = parallelizeCollection.map(x => x * x).reduce((x, y) => x + y)
 
     val nlines = sc.parallelize(List("hello world", "hi"))
     val nwords = nlines.flatMap(line => line.split(" ")).collect()
 
+
+
     var wordsHashCode = words.map(word => (word.hashCode(), 1))
-    //wordsHashCode.combineByKey()
-    val el1 = new Tuple2(1, 2)
-    val el2 = new Tuple2(3, 4)
-    //sc.parallelize()
 
 
-    println(nwords(2))
-
-
-
+    //use persist to store a RDD
     val result = parallelizeCollection.map(x => x * x)
     result.persist(StorageLevel.DISK_ONLY)
     println(result.count())
     println(result.collect().mkString(","))
 
-
-
-
-    //    println(sumSquareValues)
-    //    println(firstWord)
-    //    println(items(1)._1)
-    //    println(items(1)._2)
-    //    println(noOfItems)
-
-    // Save the word count back out to a text file, causing evaluation.
-    //counts.saveAsTextFile(outputFile)
-    println("Ana are mere")
   }
 
 
+  // understanding combine by key
   private def testCombineByKey(sc: SparkContext): Unit = {
 
 
@@ -97,9 +88,9 @@ object AppSpark {
     scoresWithKeyRDD.foreachPartition(partition => println(partition.length))
 
     val avgScoresRDD = scoresWithKeyRDD.combineByKey(
-      (x: ScoreDetail) => (x.score, 1),
-      (acc: (Float, Int), x: ScoreDetail) => (acc._1 + x.score, acc._2 + 1),
-      (acc1: (Float, Int), acc2: (Float, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+      (x: ScoreDetail) => (x.score, 1), // combiner
+      (acc: (Float, Int), x: ScoreDetail) => (acc._1 + x.score, acc._2 + 1), // merge values step
+      (acc1: (Float, Int), acc2: (Float, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)) // merge combiners steps
       .map(
         {
           case (key, value) => (key, value._1 / value._2)
@@ -108,33 +99,18 @@ object AppSpark {
 
     val values = avgScoresRDD.collect().foreach(item => println(item._1 + " -" + item._2))
 
-
-    val avgScoresRDD2 = scoresWithKeyRDD.combineByKey(
-      (x: ScoreDetail) => (x.score, 1),
-      (acc: (Float, Int), x: ScoreDetail) => (acc._1 * (acc._2 / acc._2 + 1) + (x.score / (acc._2 + 1)), acc._2 + 1),
-      (acc1: (Float, Int), acc2: (Float, Int)) => (acc1._1 * acc1._2 / (acc1._2 + acc2._2) + acc1._2 * acc2._2 / (acc1._2 + acc2._2), acc1._2 + acc2._2))
-
-    val values2 = avgScoresRDD2.collect().foreach(item => println(item._1 + " -" + item._2._1))
-
-
-    val avgScoresRDD3 = scoresWithKeyRDD.combineByKey(
-      (x: ScoreDetail) => (x.score, 1),
-      (acc: (Float, Int), x: ScoreDetail) => (acc._1 + x.score, acc._2 + 1),
-      (acc1: (Float, Int), acc2: (Float, Int)) => (acc2._1 * (acc2._2.toFloat / (acc1._2 + acc2._2).toFloat) + (acc1._1 / (acc1._2 + acc2._2)), acc1._2 + acc2._2)
-    )
-
-
-    val values3 = avgScoresRDD3.collect().foreach(item => println(item._1 + " - " + item._2._1 + " - " + item._2._2))
-
   }
 
 
-  private def testDegreeOfParalellism(sc: SparkContext): Unit = {
+  //test degree of parallelism
+  private def testDegreeOfParallelism(sc: SparkContext): Unit = {
     val data = Seq(("a", 3), ("b", 4), ("a", 1))
     sc.parallelize(data).reduceByKey((x, y) => x + y) // Default parallelism
     sc.parallelize(data).reduceByKey((x, y) => x + y, 10) // Custom parallelism
   }
 
+
+  // test using GroupByKey
   private def testUsingGroupByKey(sc: SparkContext): Unit = {
 
     val input = sc.textFile("c://spark//readme.md")
@@ -145,14 +121,14 @@ object AppSpark {
     val firstItemVal = firstItem;
     val items = List(firstItemVal)
     val countsWord = words.map(word => (word, 1))
+
     val countsWordWithP = words.filter(item => item.startsWith("p")).map(word => (word, 1))
-
     val ret = countsWord.leftOuterJoin(countsWordWithP).collect()
-
     println(items(0))
 
   }
 
+  // test join RDD
   private def testJoins(sc: SparkContext): Unit = {
 
     val input = sc.textFile("c://spark//readme.md")
@@ -167,6 +143,7 @@ object AppSpark {
 
   }
 
+  // test pageRank implementation
   private def computePageRank(file: String, sc: SparkContext): Unit = {
 
     val lines = sc.textFile(file)
@@ -192,6 +169,8 @@ object AppSpark {
     ranks.foreach(item => println(item))
   }
 
+
+  // test compute averga
   private def testAverage(sparkContext: SparkContext): Unit = {
     val lines = sparkContext.textFile("c://spark//readme.md")
     val dddd = lines.zipWithIndex().map(item => item._2.toDouble)
